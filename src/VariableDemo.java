c7fef07d822e6359b2f91c0d98608252a65120d2
Daniel Pausackerl <daniel.pausackerl@edu.campus02.at>
2015-10-30 20:45:40 +0100

    Beispiel 1 - Vorname und Nachname

---
+++ src/VariableDemo.java
@@ -0,0 +1,11 @@
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Der Name lautet: Daniel Pausackerl");
+
+	}
+
+}
